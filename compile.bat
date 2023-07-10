ddd531f84734f3aee2c7f21473c756834ce2eb12
matheus-1618 <matheus1207live@gmail.com>
2023-07-09 22:49:50 -0300

    Modify batch file

--- compile.bat
+++ compile.bat
@@ -1,1 +1,3 @@
-cl /EHsc /I"C:\Users\Matheus\Downloads\Detours-4.0.1\include" main.c /link /LIBPATH:"C:\Users\Matheus\Downloads\Detours-4.0.1\lib.X64" detours.lib
+cd "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\" 
+vcvarsall.bat x86_amd64
+cl /EHsc /I"C:\Users\Matheus\Downloads\Detours-4.0.1\src" main.c /link /LIBPATH:"C:\Users\Matheus\Downloads\Detours-4.0.1\lib.X64" detours.lib
