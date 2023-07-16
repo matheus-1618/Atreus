8255fc4ea5be7a76ebe81a710fd29ba371e1a642
matheus-1618 <matheus1207live@gmail.com>
2023-07-16 16:19:06 -0300

    Change detection to CreateRemoteThread

--- sysmon_read.py
+++ sysmon_read.py
@@ -1,7 +1,7 @@
 import win32evtlog
 import xmltodict
 #eventvwr.msc
-def SearchEvents(LogName, EventId, count=1):
+def SearchEvents(LogName, EventId, count=20):
     EventLog = win32evtlog.EvtOpenLog(LogName, 1, None)
 
     #totalRecords = win32evtlog.EvtGetLogInfo(EventLog, win32evtlog.EvtLogNumberOfLogRecords)[0]
@@ -11,19 +11,21 @@ def SearchEvents(LogName, EventId, count=1):
     for evt in win32evtlog.EvtNext(ResultSet, count):
         res = xmltodict.parse(win32evtlog.EvtRender(evt, 1))
         #took attention in events like 8. Take in mind CreateRemoteThread
-        print(res["Event"]["System"]["EventID"])
-        EventData = {}
-        for e in res['Event']['EventData']['Data']:
-            if '#text' in e:
-                EventData[e['@Name']] = e['#text']
+        eventid = int(res["Event"]["System"]["EventID"])
+        if eventid in [8,11]:
+            EventData = {}
+            for e in res['Event']['EventData']['Data']:
+                if '#text' in e:
+                    EventData[e['@Name']] = e['#text']
 
         EventList.append(EventData)
 
     return EventList
 
-for i in range(0,500):
+for i in [8,11]:
     try:
         Events = SearchEvents('Microsoft-Windows-Sysmon/Operational', i)
-        #print(Events)
+        print(Events)
+        print()
     except:
         pass
