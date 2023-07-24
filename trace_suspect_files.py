71ca5ee7eb968e57ad38468583c117a596a53100
matheus-1618 <matheus1207live@gmail.com>
2023-07-23 22:27:04 -0300

    Fix trace suspect files

--- trace_suspect_files.py
+++ trace_suspect_files.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox
 from PIL import Image, ImageTk
 import psutil
 import ctypes
@@ -7,8 +6,8 @@ import os
 from controller import Controller
 import schedule
 import time
-import threading
 import json
+import multiprocessing
 
 controller = Controller()
 
@@ -32,7 +31,7 @@ def remove(files):
             print(f"Access denied. You may need administrator privileges to delete the file.")
         except Exception as e:
             print(f"An error occurred while deleting the file: {e}")
-    clear_json_file("suspected_files.json")  
+    clear_json_file("suspected_files.json")
 
 def scan_files():
     controller.scan_files()
@@ -50,7 +49,7 @@ def show_popup(files):
 
     # Set the window size and position it in the center of the screen
     window_width = 400
-    window_height = 220
+    window_height = 290
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x = (screen_width - window_width) // 2
@@ -73,7 +72,7 @@ def show_popup(files):
 
     # Function to close the popup window and call the remove function
     def on_remove():
-        remove()
+        remove(files)
         root.destroy()
 
     # Create a frame to hold the label and image
@@ -85,7 +84,7 @@ def show_popup(files):
     image_label.pack()
 
     # Create the label with the message and set the font
-    label = tk.Label(frame, text=f"Atreus discovered suspect files:\n{files}\nWhat you want to do?", font=("Arial", 12))
+    label = tk.Label(frame, text=f"Atreus discovered suspect files:\n\n{files}\n\nWhat you want to do?", font=("Arial", 12))
     label.pack(pady=10)
 
     # Create a frame to hold the buttons
@@ -103,15 +102,14 @@ def show_popup(files):
     # Run the Tkinter main loop
     root.mainloop()
 
-
 def run_job():
-    # Schedule the job to run every 3 minutes
-    schedule.every(3).minutes.do(scan_files)
+    # Schedule the job to run every 1 minutes
+    schedule.every(1).minutes.do(scan_files)
 
     # Run the job continuously
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(.1)
 
 def read_json_file(filename):
     try:
@@ -127,7 +125,7 @@ def read_json_file(filename):
     except json.JSONDecodeError:
         print(f"Error decoding JSON from file '{filename}'.")
         return False
-    
+
 def clear_json_file(filename):
     try:
         with open(filename, "w") as file:
@@ -136,17 +134,17 @@ def clear_json_file(filename):
     except Exception as e:
         print(f"Error clearing JSON file '{filename}': {e}")
         return False
-    
+
 if __name__ == "__main__":
     hide_console_window()
     high_privileges()
-    # Create a new thread for the job
-    job_thread = threading.Thread(target=run_job)
-    # Start the thread
-    job_thread.start()
+
+    # Create a new process for the job
+    job_process = multiprocessing.Process(target=run_job)
+    # Start the process
+    job_process.start()
 
     while True:
         suspect_files = read_json_file("suspected_files.json")
         if suspect_files:
             show_popup(list_to_string_with_newlines(list(suspect_files.keys())))
-
