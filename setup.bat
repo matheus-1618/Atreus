e699b9fdeb0518eb5351559133b2b73e4822f3ec
matheus-1618 <matheus1207live@gmail.com>
2023-07-25 22:21:50 -0300

    Fix setup

--- setup.bat
+++ setup.bat
@@ -4,7 +4,6 @@ REM Step 1: Install virtualenv via pip
 pip install virtualenv
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the installation of virtualenv.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
@@ -12,7 +11,6 @@ REM Step 2: Create a virtual environment named "env"
 python -m virtualenv env
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the creation of the virtual environment.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
@@ -20,7 +18,6 @@ REM Step 3: Activate the virtual environment
 call env\Scripts\activate
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred while activating the virtual environment.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
@@ -28,7 +25,6 @@ REM Step 4: Install packages from requirements.txt
 pip install -r requirements.txt
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the installation of packages from requirements.txt.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
@@ -36,35 +32,30 @@ REM Step 5: Run PyInstaller for specific .py files
 pyinstaller --noconsole --onefile --distpath . --icon assets\atreus.png src\install.py
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the PyInstaller build for install.py.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
 pyinstaller --noconsole --onefile --distpath . --icon assets\atreus.png src\trace_registry.py
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the PyInstaller build for trace_registry.py.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
 pyinstaller --noconsole --onefile --distpath . --icon assets\atreus.png src\watchdog.py
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the PyInstaller build for watchdog.py.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
 pyinstaller --noconsole --onefile --distpath . --icon assets\atreus.png src\trace_suspect_files.py
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the PyInstaller build for trace_suspect_files.py.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
 pyinstaller --noconsole --onefile --distpath . --icon assets\atreus.png src\complex_interface.py
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred during the PyInstaller build for complex_interface.py.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
@@ -72,7 +63,6 @@ REM Step 6: Delete the build folder
 rmdir /s /q build
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred while deleting the build folder.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
@@ -85,7 +75,6 @@ REM Step 8: Deactivate the virtual environment (optional, if you want to exit th
 deactivate
 IF %ERRORLEVEL% NEQ 0 (
     echo An error occurred while deactivating the virtual environment.
-    REM You can add further actions or error handling here if needed.
     exit /b 1
 )
 
