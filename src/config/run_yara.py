6fd1652d910bc122f7fc18a205f196bd03fa86a0
matheus-1618 <matheus1207live@gmail.com>
2023-07-28 00:25:38 -0300

    Fix Yara run file

--- src/config/run_yara.py
+++ src/config/run_yara.py
@@ -3,12 +3,12 @@ import os
 
 def scan_exe_yara(directories):
     files = {}
-    rules = yara.compile(r"rule.yar")
+    rules = yara.compile(r"config\rule.yar")
     for target_directory in directories:
         for file_name in os.listdir(target_directory):
             file_path = os.path.join(target_directory, file_name)
             if os.path.isfile(file_path):
                 matches = rules.match(file_path)
                 if matches:
-                    files[os.path.join(target_directory, file_name)] = matches
+                    files[os.path.join(target_directory, file_name)] = str(matches[0])
     return files
